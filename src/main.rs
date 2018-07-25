69bd5e44eae8828269d2f76fe4d86ceb98931206
adilson <adilson.ajpj@gmail.com>
2018-07-24 22:19:34 -0300

    Fixed CREATE TABLE error; Add Menu Function -> Ask a select option to user

--- src/main.rs
+++ src/main.rs
@@ -5,56 +5,71 @@ use postgres::{Connection, TlsMode};
 
 
 fn main() {
-    println!("Bem vindo ao programa que vai te deixar mais organizado!");
-    println!("Primeiramente vamos precisar das credenciais para acessar o banco de dados PostgreSQL!");
-    //let link: String = get_db_credentials();
-    let link = "postgresql://adilson:teste@localhost:5432/adilson";
-    let connection = Connection::connect(link, TlsMode::None).unwrap();
-    // Querys utilizadas
-    let starter_query = "CREATE TABLE todolist IF NOT EXISTS(\
-    id SERIAL PRIMARY KEY,\
-    task VARCHAR(300) NOT NULL)";
-    let show_tasks_query = "SELECT task FROM todolist";
+    // Iniciando programa
+        println!("Bem vindo ao programa que vai te deixar mais organizado!");
+        println!("Primeiramente vamos precisar das credenciais para acessar o banco de dados PostgreSQL!");
+
+    // Criando os links de acesso ao BD
+        //let link: String = get_db_credentials();
+        let link = "postgresql://adilson:teste@localhost:5432/adilson";
+        let connection = Connection::connect(link, TlsMode::None).unwrap();
+
+    // Criando querys que serão utilizadas
+
+        let starter_query = "CREATE TABLE IF NOT EXISTS todolist(
+        id SERIAL PRIMARY KEY,
+        task VARCHAR(300) NOT NULL)";
+
+        let show_tasks_query = "SELECT task FROM todolist";
+
     // Executando as querys
-    connection.execute(starter_query,&[]).unwrap();
-    print!("Criou OK!");
+        connection.execute(starter_query,&[]).unwrap();
     
-    //let resultado = connection.query(query,&[]);
-    for i in &connection.query(show_tasks_query,&[]).unwrap(){
-        let row:String = i.get("task");
-        println!("{}",row);
-    }
-    print!("Tchau!")
+        //let resultado = connection.query(query,&[]);
+        for i in &connection.query(show_tasks_query,&[]).unwrap(){
+            let row:String = i.get("task");
+            println!("{}",row);
+        }
+
 }
 fn get_db_credentials() -> String{
-    // Função para retornar a url de conexão pro módulo postgres
     // Definição das variáveis
-    let mut db_adress = String::new();
-    let mut db_name = String::new();
-    let mut db_user = String::new();
-    let mut db_user_password = String::new();
+        let mut db_adress = String::new();
+        let mut db_name = String::new();
+        let mut db_user = String::new();
+        let mut db_user_password = String::new();
+        let mut param: String = "postgresql://".to_owned();
     // Obtenção dos dados
-    println!("Insira o endereço do banco de dados:");
-    db_adress = read!();
-    println!("Insira o nome do banco de dados:");
-    db_name = read!();
-    println!("Insira o nome de usuário do banco de dados:");
-    db_user = read!();
-    println!("Insira a senha do usuário:");
-    db_user_password = read!();
-    let mut param: String = "postgresql://".to_owned();
+        println!("Insira o endereço do banco de dados:");
+        db_adress = read!();
+        println!("Insira o nome do banco de dados:");
+        db_name = read!();
+        println!("Insira o nome de usuário do banco de dados:");
+        db_user = read!();
+        println!("Insira a senha do usuário:");
+        db_user_password = read!();
     // Criando o link de acesso ao DB
-    param.push_str(&db_user);
-    param.push_str(":");
-    param.push_str(&db_user_password);
-    param.push_str("@");
-    param.push_str(&db_adress);
-    param.push_str(":5432");
-    param.push_str("/");
-    param.push_str(&db_name);
-    param
-}
+        param.push_str(&db_user);
+        param.push_str(":");
+        param.push_str(&db_user_password);
+        param.push_str("@");
+        param.push_str(&db_adress);
+        param.push_str(":5432");
+        param.push_str("/");
+        param.push_str(&db_name);
+    // Retornando a variável
+        param
+} // Get the database credentials to remote/local access
 
-fn application_menu(){
-
-}
+fn application_menu()  -> i8 {
+    // Imprimindo o MENU
+        println!("Selecione uma opção:");
+        println!("1 - Listar tarefas");
+        println!("2 - Inserir tarefa");
+        println!("3 - Excluir tarefa");
+        println!("4 - Sair do programa");
+    // Lendo a opção
+        let answer = read!();
+    // Retornando a opção selecionada
+        answer
+} // Return a selected option
